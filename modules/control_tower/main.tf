c4dffec1bc95eb2a3a2043cccf9b7bc88430a679
Daniel Okoye <okoyedann@gmail.com>
2025-03-03 17:04:59 +0100

    Update control_identifier in control_tower module to use full ARN format

--- modules/control_tower/main.tf
+++ modules/control_tower/main.tf
@@ -3,6 +3,6 @@ resource "aws_organizations_organization" "main" {
 }
 
 resource "aws_controltower_control" "baseline" {
-  control_identifier  = "guardrails/mandatory"
+  control_identifier  = "arn:aws:controltower:us-east-1::control/guardrails/mandatory"
   target_identifier   = "arn:aws:organizations::123456789012:ou/o-exampleorgid/ou-exampleouid"
 }
